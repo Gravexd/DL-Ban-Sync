0956bd5399bfecfd55972fcb235d08cdaed3f787
Gravxd <mikey643214@gmail.com>
2025-02-01 11:37:38 -0500

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+Config.DebugMode = false
+Config.BanThread = false -- This might hit ratelimits at higher playercounts
+
+Config.Setup = {
+    BotToken = "your token here",
+    GuildId = "your guild id here",
+    LogWebhook = "your webhook url here", -- Leave blank to turn logs off
+    BanMessage = "You are banned from our discord so you can not join our FiveM server."
+}
